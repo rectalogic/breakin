cbb2e9065a4d5df3fc95b30f3ca9598c183ddb58
Andrew Wason <rectalogic@rectalogic.com>
2025-03-19 12:17:52 -0400

    semi working arcball

---
+++ src/arcball.rs
@@ -0,0 +1,41 @@
+// use bevy::ecs::change_detection::Mut;
+use bevy::prelude::*;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(Update, arcball);
+}
+
+// https://github.com/roy-t/roy-t.nl/blob/master/_posts/2010-02-21-xna-simple-arcballcamera.md
+#[derive(Component, Default)]
+#[require(Transform)]
+pub(super) struct ArcBallController {
+    pub look_at: Vec3,
+    pub distance: f32,
+    pub pitch: f32,
+    pub yaw: f32,
+}
+
+fn arcball(mut controller_query: Query<(&ArcBallController, Mut<Transform>)>) {
+    let Ok((controller, mut transform)) = controller_query.get_single_mut() else {
+        return;
+    };
+    if !transform.is_changed() {
+        return;
+    }
+    // Calculate the relative position of the camera
+    let mut position = Transform::from_rotation(Quat::from_euler(
+        EulerRot::YXZ,
+        controller.yaw,
+        controller.pitch,
+        0.0,
+    ))
+    .back()
+    .as_vec3();
+
+    // Convert the relative position to the absolute position
+    position *= -controller.distance;
+    position += controller.look_at;
+
+    transform.translation = position;
+    transform.look_at(controller.look_at, Vec3::Y);
+}
--- src/player.rs
+++ src/player.rs
@@ -1,16 +1,40 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
+
+use crate::arcball;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Startup, setup);
+    app.add_plugins(arcball::plugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, arcball);
 }
 
 fn setup(mut commands: Commands) {
     //XXX make these children of the camera, so as we move we shine light at cube
     //XXX also need a collider that covers the "near" plane
+    info!("Setting up player"); //XXX
     commands
         .spawn((
+            arcball::ArcBallController {
+                distance: 17.0,
+                ..default()
+            },
             Camera3d::default(),
-            Transform::from_xyz(0.0, 0.0, -17.0).looking_at(Vec3::ZERO, Dir3::Y),
         ))
         .with_child(PointLight::default());
 }
+
+//XXX need ArcBall camera
+// https://github.com/roy-t/roy-t.nl/blob/caaf939b776b673b47a5bb5af89233c3adabce79/_posts/2010-02-21-xna-simple-arcballcamera.md#L4
+fn arcball(
+    // mut commands: Commands,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+) {
+    if !mouse_buttons.pressed(MouseButton::Left) {
+        return;
+    }
+
+    let delta = mouse_motion.delta;
+    dbg!(delta);
+}
+//XXX need orbit system - use mouse_motion: Res<AccumulatedMouseMotion>, and map 180 degrees to half width/height of viewport?
