a83868c310d929f4266ee8ca29708a1bdb940b84
Andrew Wason <rectalogic@rectalogic.com>
2025-03-22 16:34:23 -0400

    size paddle to frustum

--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,9 @@
 use avian3d::prelude::*;
 use bevy::{
     input::{common_conditions::input_just_pressed, mouse::AccumulatedMouseMotion},
-    pbr::wireframe::{Wireframe, WireframePlugin},
     prelude::*,
+    render::camera::CameraProjection,
+    window::WindowResized,
 };
 use std::f32::consts::FRAC_PI_4;
 
@@ -10,13 +11,12 @@ use crate::{app, arcball, ball, bricks};
 
 const SQRT_3: f32 = 1.73205_f32;
 const PADDLE_Z_LENGTH: f32 = 1.0;
-const PADDLE_SIZE: f32 = ball::BALL_RADIUS * 1.5;
 // Radius of sphere enclosing cube of side N is (N√3)/2
 const ENCLOSING_RADIUS: f32 = (bricks::CUBE_SIZE as f32 * SQRT_3) / 2.0;
 pub(super) const PLAYFIELD_RADIUS: f32 = ENCLOSING_RADIUS * 2.0;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((WireframePlugin, arcball::plugin))
+    app.add_plugins(arcball::plugin)
         .add_systems(Startup, setup)
         .add_systems(
             OnTransition {
@@ -28,6 +28,7 @@ pub(super) fn plugin(app: &mut App) {
         .add_systems(
             Update,
             (
+                on_resize,
                 move_player,
                 fire_ball.run_if(
                     input_just_pressed(KeyCode::Space).and(in_state(app::AppState::ReadyBall)),
@@ -47,29 +48,52 @@ struct Paddle;
 #[require(Transform)]
 pub(super) struct BallPlaceholder;
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
-    let projection = PerspectiveProjection::default();
-    let near = projection.near;
+fn setup(mut commands: Commands) {
     commands
         .spawn((
             Player,
             RigidBody::Kinematic,
             arcball::ArcBallController::new(PLAYFIELD_RADIUS),
             Camera3d::default(),
-            Projection::Perspective(projection),
+            Projection::Perspective(PerspectiveProjection::default()),
         ))
         .with_children(|parent| {
             parent.spawn(PointLight::default());
             parent.spawn((
                 Paddle,
                 Restitution::new(1.0),
-                Collider::cuboid(PADDLE_SIZE, PADDLE_SIZE, PADDLE_Z_LENGTH),
+                Collider::cuboid(1.0, 1.0, PADDLE_Z_LENGTH),
                 CollisionLayers::new(app::GameLayer::Paddle, [app::GameLayer::Ball]),
-                Wireframe,
-                Mesh3d(meshes.add(Cuboid::new(PADDLE_SIZE, PADDLE_SIZE, PADDLE_Z_LENGTH))),
-                Transform::from_xyz(0.0, 0.0, -(near + ball::BALL_RADIUS * 4.0)),
+                Transform::default(),
             ));
         });
+}
+
+fn on_resize(
+    mut commands: Commands,
+    projection: Single<&Projection, With<Player>>,
+    paddle: Single<(Entity, &mut Transform), With<Paddle>>,
+    mut resize_reader: EventReader<WindowResized>,
+) {
+    if resize_reader.read().last().is_some() {
+        let projection = projection.into_inner();
+        let near = match projection {
+            Projection::Perspective(p) => p.near,
+            Projection::Orthographic(p) => p.near,
+        } + ball::BALL_RADIUS * 4.0;
+        let corners = projection.get_frustum_corners(near, near + PADDLE_Z_LENGTH);
+        // bottom right - bottom left
+        let x_length = corners[0].x - corners[3].x;
+        // top right - bottom left
+        let y_length = corners[1].y - corners[3].y;
+        let (paddle_entity, mut paddle_transform) = paddle.into_inner();
+        paddle_transform.translation = Vec3::new(0.0, 0.0, -(near + PADDLE_Z_LENGTH / 2.0));
+        commands.entity(paddle_entity).insert(Collider::cuboid(
+            x_length,
+            y_length,
+            PADDLE_Z_LENGTH,
+        ));
+    }
 }
 
 fn create_ball_placeholder(
